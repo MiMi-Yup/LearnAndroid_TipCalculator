1ba5b1a9e1c5880b6d7e0e7ede09684ab3f1ae5e
MiMi-Yup <tuivanthiet@gmail.com>
2022-04-25 15:26:01 +0700

    use view binding

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,49 +1,47 @@
 package com.example.tipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.*
+import android.widget.CompoundButton
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.tipcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var amount: Double = 0.00
-    var tipPercent: Int = 0
+    private var binding: ActivityMainBinding? = null
+    private var amount: Double = 0.00
+    private var tipPercent: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        txtTipPercent.text = "${tipSeekBar.progress}%"
-        tipPercent = tipSeekBar.progress
+        binding?.txtTipPercent?.text = "${binding?.tipSeekBar?.progress}%"
+        tipPercent = binding!!.tipSeekBar.progress
 
-        tipSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        binding?.tipSeekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 tipPercent = p1
 
-                txtTipPercent.text = "$p1%"
+                binding?.txtTipPercent?.text = "$p1%"
 
                 setTipValue()
 
                 setTotalValue()
             }
 
-            override fun onStartTrackingTouch(p0: SeekBar?) {
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
 
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-
-            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
 
         })
 
-        edtAmount.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
+        binding?.edtAmount?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 val value: Double? = p0.toString().toDoubleOrNull()
@@ -60,12 +58,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
+            override fun afterTextChanged(p0: Editable?) {}
         })
 
-        swNightMode.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
+        binding?.swNightMode?.setOnCheckedChangeListener(object :
+            CompoundButton.OnCheckedChangeListener {
             override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
                 if (p1) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -77,16 +74,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setTipValue() {
-        txtValueTip.text = String.format("%.2f", amount * tipPercent / 100.00)
+        binding?.txtValueTip?.text = String.format("%.2f", amount * tipPercent / 100.00)
     }
 
     fun setTotalValue() {
-        txtTotalValue.text =
+        binding?.txtTotalValue?.text =
             String.format("%.2f", ((amount * tipPercent / 100.00)) + amount)
     }
 
     fun resetValue() {
-        txtValueTip.text = "Tip"
-        txtTotalValue.text = "Total"
+        binding?.txtValueTip?.text = "Tip"
+        binding?.txtTotalValue?.text = "Total"
+        amount = 0.0
     }
 }
